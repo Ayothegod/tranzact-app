8cde99ec3334a63daf003d39c894ebcfafad2555
Ayothegod <heyayomideadebisi@gmail.com>
2024-11-30 20:46:29 +0100

    refactor unneeded old code

--- server/src/routes/default.route.ts
+++ server/src/routes/default.route.ts
@@ -6,6 +6,11 @@ const router = Router();
 
 router.route("/").get(
   asyncHandler(async (req: Request, res: Response) => {
+    const ip = req.ip
+    console.log(req.ip);
+    console.log(req.clientIp);
+    
+
     return res
       .status(200)
       .json(new ApiResponse(200, "OK", "Default route working successfully", ));
--- server/src/utils/constants.ts
+++ server/src/utils/constants.ts
@@ -26,3 +26,66 @@ export const GenErrorEnum = Object.freeze({
     message: "Network issue, please retry.",
   },
 });
+
+/**
+ * @type {{ ADMIN: "ADMIN"; USER: "USER"} as const}
+ */
+export const UserRolesEnum = {
+  ADMIN: "ADMIN",
+  USER: "USER",
+};
+
+export const AvailableUserRoles = Object.values(UserRolesEnum);
+
+/**
+ * @type {{ PENDING: "PENDING"; CANCELLED: "CANCELLED"; DELIVERED: "DELIVERED" } as const}
+ */
+export const OrderStatusEnum = {
+  PENDING: "PENDING",
+  CANCELLED: "CANCELLED",
+  DELIVERED: "DELIVERED",
+};
+
+export const AvailableOrderStatuses = Object.values(OrderStatusEnum);
+
+/**
+ * @type {{ UNKNOWN:"UNKNOWN"; RAZORPAY: "RAZORPAY"; PAYPAL: "PAYPAL"; } as const}
+ */
+export const PaymentProviderEnum = {
+  UNKNOWN: "UNKNOWN",
+  RAZORPAY: "RAZORPAY",
+  PAYPAL: "PAYPAL",
+};
+
+export const AvailablePaymentProviders = Object.values(PaymentProviderEnum);
+
+/**
+ * @type {{ FLAT:"FLAT"; } as const}
+ */
+export const CouponTypeEnum = {
+  FLAT: "FLAT",
+  // PERCENTAGE: "PERCENTAGE",
+};
+
+export const AvailableCouponTypes = Object.values(CouponTypeEnum);
+
+/**
+ * @type {{ GOOGLE: "GOOGLE"; GITHUB: "GITHUB"; EMAIL_PASSWORD: "EMAIL_PASSWORD"} as const}
+ */
+export const UserLoginType = {
+  GOOGLE: "GOOGLE",
+  GITHUB: "GITHUB",
+  EMAIL_PASSWORD: "EMAIL_PASSWORD",
+};
+
+export const AvailableSocialLogins = Object.values(UserLoginType);
+
+/**
+ * @type {{ MOST_VIEWED: "mostViewed"; MOST_LIKED: "mostLiked"; LATEST: "latest"; OLDEST: "oldest"} as const}
+ */
+export const YouTubeFilterEnum = {
+  MOST_VIEWED: "mostViewed",
+  MOST_LIKED: "mostLiked",
+  LATEST: "latest",
+  OLDEST: "oldest",
+};
