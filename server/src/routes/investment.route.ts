955a110158fe0fcba1b46eec8bedfdd5a6bc9eaf
Ayothegod <heyayomideadebisi@gmail.com>
2024-12-01 21:40:16 +0100

    pause investments route for now

--- server/src/routes/investment.route.ts
+++ server/src/routes/investment.route.ts
@@ -6,7 +6,11 @@ const router = Router();
 
 router.use(verifyCookie);
 
-router.route("/category").get(validate);
+router.route("/").post(validate);
+// router.route("/").get(validate, getAllGoals);
+
+// router.route("/:id").get(validate, getGoal);
+// router.route("/:id").patch(validate, updateGoal);
+// router.route("/:id").post(validate, deleteGoal);
 
 export default router;
-
